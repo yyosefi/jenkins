885f8c3971cbaad9eae0340534d56f019d9c8ebb
Quirky Hacker <quirkyhacker@gmail.com>
2022-07-04 01:24:37 -0700

    remove code

--- vars/buildEnvironment.groovy
+++ vars/buildEnvironment.groovy
@@ -7,24 +7,6 @@ def call(Map params = null, Closure body = null) {
     println("Environment is " + cluster)
     println("componentName is " + componentName)
 
-    /*node(label) {
-        try {
-            ansiColor("xterm") {
-                if (!body) {
-                    echo "!body()"
-                } else {
-                    echo "body()"
-                    body()
-                }
-            }
-        } catch (err) {
-            // DO SOME ACTIONS HERE...
-            echo "Caught: ${err}"
-            //error(err.getMessage())
-        } finally {
-            // DO SOME ACTIONS HERE...
-        }
-    }*/
 
     pipeline {
         agent any
